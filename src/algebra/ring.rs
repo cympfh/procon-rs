1d38dd0e1308b8790c55ae937ff539001bd0b6ba
cympfh <cympfh@gmail.com>
2021-12-31 01:10:04 +0900

    :art: modint

--- src/algebra/ring.rs
+++ src/algebra/ring.rs
@@ -4,5 +4,30 @@ use crate::algebra::monoid::*;
 
 pub trait Ring: AGroup + Monoid {}
 
+#[macro_export]
+macro_rules! ring {
+    (
+        $type:ty where [ $( $params:tt )* ];
+        div($self:ident, $other:ident) = $code:block
+        $(;)*
+    ) => {
+        impl<$($params)*> std::ops::Div for $type {
+            type Output = Self;
+            fn div($self, $other: Self) -> Self { $code }
+        }
+        impl<$($params)*> std::ops::DivAssign for $type where Self: Clone {
+            fn div_assign(&mut $self, $other: Self) { *$self = (*$self).clone() / $other; }
+        }
+        impl Ring for $type {}
+    };
+    (
+        $type:ty;
+        div($self:ident, $other:ident) = $code:block
+        $(;)*
+    ) => {
+        ring! { $type where []; div($self, $other) = $code; }
+    };
+}
+
 impl Ring for i64 {}
 impl Ring for f64 {}

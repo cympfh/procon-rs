ed21902dc277160c39488e5335cb2fdd0997fc95
cympfh <cympfh@gmail.com>
2022-02-27 18:03:42 +0900

    is_prime

---
+++ src/num/prime/is_prime.rs
@@ -0,0 +1,36 @@
+use crate::num::prime::mrtest::*;
+
+pub fn is_prime(n: u64) -> bool {
+    if n == 2 {
+        true
+    } else if n < 2 || n % 2 == 0 {
+        false
+    } else {
+        for p in 2..10_000_000 {
+            if p * p > n {
+                return true;
+            }
+            if n % p == 0 {
+                return false;
+            }
+        }
+        mrtest(n)
+    }
+}
+
+#[cfg(test)]
+mod test_is_prime {
+    use crate::num::prime::is_prime::is_prime;
+
+    #[test]
+    fn it_works() {
+        let primes = vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43];
+        let not_primes = vec![0, 1, 4, 6, 8, 9, 12, 15, 21, 25, 27, 33, 35];
+        for p in primes {
+            assert!(is_prime(p));
+        }
+        for q in not_primes {
+            assert!(!is_prime(q));
+        }
+    }
+}
--- src/num/prime/mod.rs
+++ src/num/prime/mod.rs
@@ -1,3 +1,4 @@
 pub mod factorize;
+pub mod is_prime;
 pub mod mrtest;
 pub mod sieve;
